e54b930bef404cf9bb820c26eed1a1cbff83bb88
Angelo Zero <angelo.akm@gmail.com>
2020-08-13 23:11:58 -0300

    angular

---
+++ angular/cat-status-code/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './01-app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Cat Status Code';
+  //Desafio
+  url = 'https://i.pinimg.com/originals/21/4a/88/214a884a0f81de79ebfa7d6ef4e27754.jpg';
+
+}
